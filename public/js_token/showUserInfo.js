be02a8f974b4784e671fcb12f8cb2b6367b35b7e
BatShevaSofer <b0583247452@gmail.com>
2023-11-29 15:05:39 +0200

    29/11/2023 15:05

--- public/js_token/showUserInfo.js
+++ public/js_token/showUserInfo.js
@@ -15,7 +15,7 @@ const doApi = async() => {
     }
   })
   console.log(resp.data);
-  if(resp.data.id){
+  if(resp.data._id){
     let item = resp.data;
     document.querySelector("#id_name").innerHTML = item.name;
     document.querySelector("#id_email").innerHTML = item.email;
